fc822b334d4f9d49e9dd88940ce8784685eec149
jomilto <joselobatof@gmail.com>
2021-11-23 03:21:57 +0000

    first test

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,25 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+// import { mount } from '@vue/test-utils'
+// import App from '@/App.vue'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
-  })
-})
+import { huevo, espinaca } from "./jestTest";
+
+// const wrapper = mount(App)
+
+// console.log(wrapper.element)
+
+// const vm = wrapper.vm
+
+// console.log(vm.$data)
+
+describe('Se probará la calidad de los productos', () => {
+    test('Calidad del huevo', () => {
+        expect(huevo().estado[0]).toBe('fresco')
+    });
+
+    test('Calidad de la espinaca', () => {
+        expect(espinaca()).toEqual({
+            estado: 1,
+            color: "verde"
+        })
+    });
+});
